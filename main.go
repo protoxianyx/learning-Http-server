95a0ed9782c0a9f2dac74f80c034737c5ee92f07
Xianyx <pushkar44320@gmail.com>
2025-10-17 12:52:13 +0530

    file read explationation

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func main() {
 		log.Fatal("error ", "error", err) // Find the reason why does this output needs two "error"
 	}
 
-	str := ""
+	str := "" // first stream of data that is t-c | 
 	for {
 		//byte is just an alias for uint8
 		data := make([]byte, 8)
@@ -24,20 +24,23 @@ func main() {
 			break
 		}
 
-		data = data[:n]
-		if i := bytes.IndexByte(data, '\n'); i != -1 {
-			str += string(data[:i])
+		data = data[:n] // Again stores the value of the data from the start to the 8thth position. Which is T -> c | c to the 8th position upto the moto part whrer \n i sencountered
+		fmt.Printf("data data: %s\n", data)
+		if i := bytes.IndexByte(data, '\n'); i != -1 { // skips this until matches the condition aka it shoudl end in the new line. | encounetrs \n
+			str += string(data[:i])					//  at this poitn the str is the whole first line, adn the I position is the end of fthe new line 
 			data = data[i+1:]
 
 			fmt.Printf("read: %s\n", str)
+			fmt.Printf("index: %d \n", i)
 			str = ""
 		}
 
-		str += string(data)
+		str += string(data) // saves upto t - c in str before the start of the for loop. t-c |
+		fmt.Printf("str data: %s\n", str)
 	}
 
 	if len(str) != 0 {
-		fmt.Printf("read: %s\n", str)
+		fmt.Printf("second read: %s\n", str)
 
 	}
 }
