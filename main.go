b2b830f34583088ec2be8786f73b65b172e0b07e
Xianyx <pushkar44320@gmail.com>
2025-10-17 12:02:55 +0530

    http server

--- main.go
+++ main.go
@@ -1,20 +1,7 @@
 package main
 
-import (
-	"fmt"
-	"net"
-)
+import "fmt"
 
 func main() {
-	ln, _ := net.Listen("tcp", ":8080")
-	defer ln.Close()
-
-	for {
-		conn, _ := ln.Accept() // accept connection
-		go func(c net.Conn) {
-			defer c.Close()
-			fmt.Fprintf(c, "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\nHello World!")
-		}(conn)
-	}
-
+	fmt.Println("Let's go!")
 }
