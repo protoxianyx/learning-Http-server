0de03247ea57c9cc4fba5cf93adf945217bb3369
Xianyx <pushkar44320@gmail.com>
2025-10-17 12:25:39 +0530

    reading from file

--- main.go
+++ main.go
@@ -7,12 +7,15 @@ import (
 )
 
 func main() {
+
+	// reads a file and stores a error if there are any
 	f, err := os.Open("message.txt")
 	if err != nil {
-		log.Fatal("error", "error", err)
+		log.Fatal("error ", "error", err) // Find the reason why does this output needs two "error"
 	}
 
 	for {
+		//byte is just an alias for uint8
 		data := make([]byte, 8)
 		n, err := f.Read(data)
 		if err != nil {
