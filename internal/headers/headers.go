9fa95a1b194a8083b16b83aa642293dfaf9d007e
Xianyx <pushkar44320@gmail.com>
2025-10-21 14:25:40 +0530

    comstraints and validation

--- internal/headers/headers.go
+++ internal/headers/headers.go
@@ -3,16 +3,33 @@ package headers
 import (
 	"bytes"
 	"fmt"
+	"strings"
 )
 
-type Headers map[string]string
+func isToken(str []byte) bool {
+	for _, ch := range str {
+		found := false
+		if ch >= 'A' && ch <= 'Z' ||
+			ch >= 'a' && ch <= 'z' ||
+			ch >= '0' && ch <= '9' {
+			found = true
+		}
+		switch ch {
+		case 'A', 'Z', '!', '#', '$', '%', '&', '\'', '*', '+', '-', '.', '^', '_', '`', '|', '~':
+			found = true
+
+		}
+
+		if !found {
+			return false
+		}
+	}
+
+	return true
+
+}
 
 var rn = []byte("\r\n")
-
-func NewHeaders() Headers {
-
-	return map[string]string{}
-}
 
 func parseHeader(fieldLine []byte) (string, string, error) {
 	parts := bytes.SplitN(fieldLine, []byte(":"), 2)
@@ -21,14 +38,33 @@ func parseHeader(fieldLine []byte) (string, string, error) {
 	}
 
 	name := parts[0]
-	value := bytes.TrimSpace(parts[1]) 
+	value := bytes.TrimSpace(parts[1])
 	if bytes.HasSuffix(name, []byte(" ")) {
 		return "", "", fmt.Errorf("malformed field-name")
 
 	}
 
-
 	return string(name), string(value), nil
+}
+
+type Headers struct {
+	headers map[string]string
+}
+
+func NewHeaders() *Headers {
+
+	return &Headers{
+		headers: map[string]string{},
+	}
+}
+
+func (h *Headers) Get(name string) string {
+	return h.headers[strings.ToLower(name)]
+}
+
+func (h *Headers) Set(name, value string) {
+	h.headers[strings.ToLower(name)] = value
+
 }
 
 func (h Headers) Parse(data []byte) (int, bool, error) {
@@ -47,13 +83,18 @@ func (h Headers) Parse(data []byte) (int, bool, error) {
 			break
 		}
 
-		name, value, err := parseHeader(data[read:read+idx])
+		name, value, err := parseHeader(data[read : read+idx])
 		if err != nil {
 			return 0, false, err
 		}
+
+		if !isToken([]byte(name)) {
+			return 0, false, fmt.Errorf("malformed header name")
+		}
+
 		read += idx + len(rn)
-		h[name] = value
+		h.Set(name, value)
 	}
 
-	return read, done, nil 
+	return read, done, nil
 }
--- internal/headers/headers_test.go
+++ internal/headers/headers_test.go
@@ -14,13 +14,21 @@ func TestHeaderParse(t *testing.T) {
 	n, done, err := headers.Parse(data)
 	require.NoError(t, err)
 	require.NotNil(t, headers)
-	assert.Equal(t, "localhost:42069", headers["Host"])
+	assert.Equal(t, "localhost:42069", headers.Get("Host"))
 	assert.Equal(t, 25, n)
 	assert.True(t, done)
 
 	// Test: Invalid spacing header
 	headers = NewHeaders()
 	data = []byte("       Host : localhost:42069       \r\n\r\n")
+	n, done, err = headers.Parse(data)
+	require.Error(t, err)
+	assert.Equal(t, 0, n)
+	assert.False(t, done)
+
+
+	headers = NewHeaders()
+	data = []byte("H©st: localhost:42069\r\n\r\n")
 	n, done, err = headers.Parse(data)
 	require.Error(t, err)
 	assert.Equal(t, 0, n)
